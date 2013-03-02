76847c24116660244f3877e9d9a54d80b0b0e9ba
Andersen Fan <as181920@hotmail.com>
2013-03-02 11:09:54 +0800

    use layout for xml

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -8,33 +8,11 @@ class MessageController < ApplicationController
   end
 
   def talk
-    if params[:xml][:MsgType] == "text"
+    if params[:xml] and params[:xml][:MsgType] == "text"
       render "reply", :formats => :xml
     end
+    render "reply", formats: :xml
   end
-
-=begin
-  def talk
-    request.body.rewind
-    body_data =  Hash.from_xml request.body.read
-    data = body_data["xml"]
-    logger.info "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-    logger.info data
-    res = <<END_TEXT
-<xml>
-<ToUserName><![CDATA[oLUkFj8Db856hPfy-V9jYfR1Hsro]]></ToUserName>
-<FromUserName><![CDATA[gh_062972c0457c]]></FromUserName>
-<CreateTime>#{Time.now.to_i}</CreateTime>
-<MsgType><![CDATA[text]]></MsgType>
-<Content><![CDATA[abc]]></Content>
-<FuncFlag><![CDATA[0]]></FuncFlag>
-</xml> 
-END_TEXT
-    logger.info res 
-    logger.info "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-    render text: res 
-  end
-=end
 
   private
   # 根据参数校验请求是否合法，如果非法返回错误页面
