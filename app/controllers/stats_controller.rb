7a45af06a2febd80c871c614c17fc979aa78cec1
Andersen Fan <as181920@hotmail.com>
2013-05-07 17:36:05 +0800

    add users stat page

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -2,4 +2,8 @@ class StatsController < ApplicationController
   def keywords
     @wx_texts = WxText.select("content, count(content) as count").group(:content)
   end
+
+  def users
+    @users = User.all
+  end
 end
