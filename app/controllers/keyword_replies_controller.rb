4d906b214dcdf144d4e9e7fe51ea9002f4b69e17
Andersen Fan <as181920@hotmail.com>
2013-05-14 18:43:40 +0800

    关键词回复内容管理

--- app/controllers/keyword_replies_controller.rb
+++ app/controllers/keyword_replies_controller.rb
@@ -2,17 +2,46 @@
 class KeywordRepliesController < ApplicationController
 
   def index
+    @keyword_replies = KeywordReply.all
+  end
+
+  def show
+    @keyword_reply = KeywordReply.find params[:id]
   end
 
   def new
+    @keyword_reply = KeywordReply.new
   end
 
   def create
+    @keyword_reply = KeywordReply.new(params[:keyword_reply])
+
+    if @keyword_reply.save
+      #redirect_to @keyword_reply, notice: 'successfully created.'
+      redirect_to keyword_replies_path, notice: 'successfully created.'
+    else
+      render action: 'new'
+    end
   end
 
   def edit
+    @keyword_reply = KeywordReply.find params[:id]
   end
 
   def update
+    @keyword_reply = KeywordReply.find params[:id]
+
+      if @keyword_reply.update_attributes(params[:keyword_reply])
+        redirect_to @keyword_reply
+      else
+        render action: "edit"
+      end
+  end
+
+  def destroy
+    @keyword_reply = KeywordReply.find params[:id]
+    @keyword_reply.destroy
+
+    redirect_to keyword_replies_path
   end
 end
--- app/models/keyword_reply.rb
+++ app/models/keyword_reply.rb
@@ -1,3 +1,4 @@
 class KeywordReply < ActiveRecord::Base
+  attr_accessible :keyword, :reply_content
 end
 
