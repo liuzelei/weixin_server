977f8c23334f59575b274c4a43ffcd515fdbdd12
Andersen Fan <as181920@hotmail.com>
2013-05-15 11:50:43 +0800

    方法提取后临时变量的问题

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -18,7 +18,6 @@ class MessageController < ApplicationController
 
   def input_text
     #per_page = params[:per_page].present? ? params[:per_page].to_i : 19
-    req_content = params[:xml][:Content].to_s
     last_response_message = ResponseMessage.where(user_id: @current_weixin_user.id).order("created_at desc").first.try :content
     #TODO
     #if response_message match qa_steps
@@ -29,7 +28,7 @@ class MessageController < ApplicationController
     #handle_smart_talk
     #end
     qa_step = QaStep.where(question: last_response_message).first
-    keyword_reply = KeywordReply.where(keyword: req_content).first unless qa_step
+    keyword_reply = KeywordReply.where(keyword: @request_content).first unless qa_step
     if qa_step
       @content = "qa_step handling."
       logger.info "qa_step handling."
@@ -45,11 +44,11 @@ class MessageController < ApplicationController
     @content = \
       case last_response_message
       when "请输入您的微信号"
-        @current_weixin_user.update_attributes weixin_id: req_content
+        @current_weixin_user.update_attributes weixin_id: @request_content
         "请选择您的性别"
       when "请选择您的性别"
         sex = \
-          case req_content
+          case @request_content
           when 0,"0"
             true
           when 1,"1"
@@ -60,21 +59,21 @@ class MessageController < ApplicationController
         @current_weixin_user.update_attributes sex: sex
         "请选择您的年龄"
       when "请选择您的年龄"
-        @current_weixin_user.update_attributes age: req_content
+        @current_weixin_user.update_attributes age: @request_content
         "请输入您的地址"
       when "请输入您的地址"
-        @current_weixin_user.update_attributes address: req_content
+        @current_weixin_user.update_attributes address: @request_content
         "继续吧"
       else
-        case req_content
+        case @request_content
         when "ZL","zl"
           "请输入您的微信号"
         when "Hello2BizUser"
           "欢迎关注哦，输[文字]翻译，输[?文字]提问,输[ZL]填写资料:)"
         when /^\?|^？|？$|\?$/
-          answer_question req_content.to_search_string
+          answer_question @request_content.to_search_string
         else
-          translate_word req_content
+          translate_word @request_content
         end
       end
 =end
@@ -98,7 +97,7 @@ class MessageController < ApplicationController
   def input_event
     req_event = params[:xml][:Event].to_s
     @content = \
-      case req_content
+      case @request_content
       when "subscribe"
         "欢迎关注哦，输[文字]翻译，输[?文字]提问:)"
       when /unsubscribe/
@@ -135,8 +134,9 @@ class MessageController < ApplicationController
   def save_request
     RequestMessage.create \
       xml: params[:xml]
+    @request_content = params[:xml][:Content].to_s
     @current_weixin_user.wx_texts.create \
-      content: req_content
+      content: @request_content
   end
 
   # 保存响应数据到数据库
