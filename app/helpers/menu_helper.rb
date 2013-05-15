aea1cbbc871ea720f11c381f9f379075094a6677
Andersen Fan <as181920@hotmail.com>
2013-05-15 23:00:29 +0800

    图文展示列表页和基础新增页面

--- app/helpers/menu_helper.rb
+++ app/helpers/menu_helper.rb
@@ -1,4 +1,8 @@
 module MenuHelper
+  def current_nav_item_state(controller="")
+    "active" if controller==params["controller"]
+  end
+
   def current_menu_item_state(action="",controller="")
     if controller.present?
       "active" if action==params[:action] and controller==params[:controller]
--- app/models/news.rb
+++ app/models/news.rb
@@ -2,4 +2,6 @@ class News < ActiveRecord::Base
   attr_accessible :description, :pic_url, :title, :url
 
   has_many :items
+
+  validates_presence_of :title, :pic_url, :url, :description
 end
