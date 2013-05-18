e58ba8b3315c3b2afcb443cfe6ad8629d645b4d4
Andersen Fan <as181920@hotmail.com>
2013-05-18 17:23:47 +0800

    one request message only has one data not has many

--- app/models/request_message.rb
+++ app/models/request_message.rb
@@ -1,11 +1,11 @@
 class RequestMessage < ActiveRecord::Base
   attr_accessible :xml, :weixin_user_id, :msg_type
 
-  has_many :wx_texts
-  has_many :wx_locations
-  has_many :wx_images
-  has_many :wx_links
-  has_many :wx_events
+  has_one :wx_texts
+  has_one :wx_locations
+  has_one :wx_images
+  has_one :wx_links
+  has_one :wx_events
 
   belongs_to :weixin_user
 end
