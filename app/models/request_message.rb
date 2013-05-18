633bc87e7596ede029aaae0b56b93ac651e2b510
Andersen Fan <as181920@hotmail.com>
2013-05-18 17:33:32 +0800

    has_one with singularize record

--- app/models/request_message.rb
+++ app/models/request_message.rb
@@ -1,11 +1,11 @@
 class RequestMessage < ActiveRecord::Base
   attr_accessible :xml, :weixin_user_id, :msg_type
 
-  has_one :wx_texts
-  has_one :wx_locations
-  has_one :wx_images
-  has_one :wx_links
-  has_one :wx_events
+  has_one :wx_text
+  has_one :wx_location
+  has_one :wx_image
+  has_one :wx_link
+  has_one :wx_event
 
   belongs_to :weixin_user
 end
