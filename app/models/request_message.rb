4988a7507f6d2a7b799b2351e3a240b609b8b5f8
Andersen Fan <as181920@hotmail.com>
2013-05-18 16:29:00 +0800

    bug: has many syntax

--- app/models/request_message.rb
+++ app/models/request_message.rb
@@ -1,6 +1,11 @@
 class RequestMessage < ActiveRecord::Base
   attr_accessible :xml, :weixin_user_id, :msg_type
 
-  has_many :wx_texts, :wx_locations, :wx_images, :wx_links, :wx_events
+  has_many :wx_texts
+  has_many :wx_locations
+  has_many :wx_images
+  has_many :wx_links
+  has_many :wx_events
+
   belongs_to :weixin_user
 end
