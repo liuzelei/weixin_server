60efb977e6e0a1f5618918f992855950494c3f08
Andersen Fan <as181920@hotmail.com>
2013-05-22 17:30:48 +0800

    可设置欢迎语

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -3,6 +3,7 @@ class SettingsController < ApplicationController
   # GET /settings.json
   def index
     #@settings = Setting.all
+    @welcome_message = Setting.find_by_name("welcome_message") || Setting.new(name: "welcome_message")
 
     respond_to do |format|
       format.html # index.html.erb
@@ -44,7 +45,8 @@ class SettingsController < ApplicationController
 
     respond_to do |format|
       if @setting.save
-        format.html { redirect_to @setting, notice: 'Setting was successfully created.' }
+        format.html { redirect_to settings_path, notice: 'Setting was successfully created.' }
+        #format.html { redirect_to @setting, notice: 'Setting was successfully created.' }
         format.json { render json: @setting, status: :created, location: @setting }
       else
         format.html { render action: "new" }
@@ -60,7 +62,8 @@ class SettingsController < ApplicationController
 
     respond_to do |format|
       if @setting.update_attributes(params[:setting])
-        format.html { redirect_to @setting, notice: 'Setting was successfully updated.' }
+        format.html { redirect_to settings_path, notice: 'Setting was successfully updated.' }
+        #format.html { redirect_to @setting, notice: 'Setting was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
