5a1df89aa461922fb632fcc7e6f6e2f07585a27f
Andersen Fan <as181920@hotmail.com>
2013-06-03 13:12:54 +0800

    rm sidekiq

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,13 +21,13 @@ set :deploy_env, 'production'
 #set :bundle_cmd, proc {"cd #{current_release} && bundle"}
 set :bundle_cmd, 'source $HOME/.bash_profile && bundle'
 
-require 'sidekiq/capistrano'
-set :sidekiq_cmd, "#{bundle_cmd} exec sidekiq"
-set :sidekiqctl_cmd, "#{bundle_cmd} exec sidekiqctl"
-set :sidekiq_timeout, 1000
-set :sidekiq_role, :app
-set :sidekiq_pid, "#{current_path}/tmp/pids/sidekiq.pid"
-set :sidekiq_processes, 3
+#require 'sidekiq/capistrano'
+#set :sidekiq_cmd, "#{bundle_cmd} exec sidekiq"
+#set :sidekiqctl_cmd, "#{bundle_cmd} exec sidekiqctl"
+#set :sidekiq_timeout, 1000
+#set :sidekiq_role, :app
+#set :sidekiq_pid, "#{current_path}/tmp/pids/sidekiq.pid"
+#set :sidekiq_processes, 3
 
 set :keep_releases, 15
 
