da1c1d61cc35868c7eb44f3edb008a9d044da5b6
Andersen Fan <as181920@hotmail.com>
2013-06-09 14:36:34 +0800

    comment unneeded code for ownership

--- app/models/ownership.rb
+++ app/models/ownership.rb
@@ -3,7 +3,7 @@ class Ownership < ActiveRecord::Base
   attr_accessible :user_id, :item_id, :item_type
 
   belongs_to :user
-  belongs_to :item, polymorphic: true, :primary_key => :id
+  belongs_to :item, polymorphic: true#, :primary_key => :id
 
 
   validates_presence_of :item_id, :item_type
