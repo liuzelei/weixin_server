0068095934f73d7ce5225d5b08b446a7bee50b3b
Andersen Fan <as181920@hotmail.com>
2013-06-09 17:52:56 +0800

    优化关键词回复关联关系，直接关联回复对象

--- app/models/keyword_reply.rb
+++ app/models/keyword_reply.rb
@@ -3,6 +3,11 @@ class KeywordReply < ActiveRecord::Base
   attr_accessible :keyword, :reply_content, :news_id, :coupon, :news_ids, :replies_attributes
 
   has_many :replies, dependent: :destroy
+  ItemTypes = ["ReplyText","News","Audio","Activity"]
+  ItemTypes.each do |item_type|
+    has_many item_type.underscore.pluralize.to_sym, through: :replies, source: "item", source_type: item_type
+  end
+
   has_one :ownership, as: :item, dependent: :destroy
   has_one :user, through: :ownerships
   accepts_nested_attributes_for :replies, allow_destroy: true
