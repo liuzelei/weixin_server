cd4117c3b1623321c81254b080486f64b4ba5a2c
Andersen Fan <as181920@hotmail.com>
2013-06-10 10:36:45 +0800

    绑定客户帐号和对应的文本素材

--- app/controllers/reply_texts_controller.rb
+++ app/controllers/reply_texts_controller.rb
@@ -3,7 +3,7 @@ class ReplyTextsController < ApplicationController
   # GET /reply_texts.json
   def index
     @per_page = params[:per_page].present? ? params[:per_page].to_i : 15
-    @reply_texts = ReplyText.order("updated_at desc").page([params[:page].to_i,1].max).per(@per_page)
+    @reply_texts = current_user.reply_texts.order("updated_at desc").page([params[:page].to_i,1].max).per(@per_page)
 
     respond_to do |format|
       format.html # index.html.erb
@@ -14,7 +14,7 @@ class ReplyTextsController < ApplicationController
   # GET /reply_texts/1
   # GET /reply_texts/1.json
   def show
-    @reply_text = ReplyText.find(params[:id])
+    @reply_text = current_user.reply_texts.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
@@ -25,7 +25,7 @@ class ReplyTextsController < ApplicationController
   # GET /reply_texts/new
   # GET /reply_texts/new.json
   def new
-    @reply_text = ReplyText.new
+    @reply_text = current_user.reply_texts.new
 
     respond_to do |format|
       format.html # new.html.erb
@@ -35,16 +35,16 @@ class ReplyTextsController < ApplicationController
 
   # GET /reply_texts/1/edit
   def edit
-    @reply_text = ReplyText.find(params[:id])
+    @reply_text = current_user.reply_texts.find(params[:id])
   end
 
   # POST /reply_texts
   # POST /reply_texts.json
   def create
-    @reply_text = ReplyText.new(params[:reply_text])
+    @reply_text = current_user.reply_texts.new(params[:reply_text])
 
     respond_to do |format|
-      if @reply_text.save
+      if current_user.save
         format.html { redirect_to @reply_text, notice: 'Reply text was successfully created.' }
         format.json { render json: @reply_text, status: :created, location: @reply_text }
       else
@@ -57,7 +57,7 @@ class ReplyTextsController < ApplicationController
   # PUT /reply_texts/1
   # PUT /reply_texts/1.json
   def update
-    @reply_text = ReplyText.find(params[:id])
+    @reply_text = current_user.reply_texts.find(params[:id])
 
     respond_to do |format|
       if @reply_text.update_attributes(params[:reply_text])
@@ -73,7 +73,7 @@ class ReplyTextsController < ApplicationController
   # DELETE /reply_texts/1
   # DELETE /reply_texts/1.json
   def destroy
-    @reply_text = ReplyText.find(params[:id])
+    @reply_text = current_user.reply_texts.find(params[:id])
     @reply_text.destroy
 
     respond_to do |format|
