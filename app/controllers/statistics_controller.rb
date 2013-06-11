100c067db61a676b69fe63b6051b1fd018c5265a
Andersen Fan <as181920@hotmail.com>
2013-06-11 09:52:00 +0800

    微调设置页html，首页验证是否已完成设置

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,5 +1,6 @@
 # encoding: utf-8
 class StatisticsController < ApplicationController
+  before_filter :check_settings, only: [:chart_messages]
 
   def msg_types
     @stats = current_user.request_messages.select("msg_type, count(msg_type) as cnt").group(:msg_type).order("cnt desc")
@@ -144,76 +145,8 @@ class StatisticsController < ApplicationController
 
   end
 
-=begin
-  # GET /statistics/1
-  # GET /statistics/1.json
-  def show
-    @statistic = Statistic.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @statistic }
-    end
+  private
+  def check_settings
+    redirect_to settings_path unless current_user.setting.present?
   end
-
-  # GET /statistics/new
-  # GET /statistics/new.json
-  def new
-    @statistic = Statistic.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @statistic }
-    end
-  end
-
-  # GET /statistics/1/edit
-  def edit
-    @statistic = Statistic.find(params[:id])
-  end
-
-  # POST /statistics
-  # POST /statistics.json
-  def create
-    @statistic = Statistic.new(params[:statistic])
-
-    respond_to do |format|
-      if @statistic.save
-        format.html { redirect_to @statistic, notice: 'Statistic was successfully created.' }
-        format.json { render json: @statistic, status: :created, location: @statistic }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @statistic.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /statistics/1
-  # PUT /statistics/1.json
-  def update
-    @statistic = Statistic.find(params[:id])
-
-    respond_to do |format|
-      if @statistic.update_attributes(params[:statistic])
-        format.html { redirect_to @statistic, notice: 'Statistic was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @statistic.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /statistics/1
-  # DELETE /statistics/1.json
-  def destroy
-    @statistic = Statistic.find(params[:id])
-    @statistic.destroy
-
-    respond_to do |format|
-      format.html { redirect_to statistics_url }
-      format.json { head :no_content }
-    end
-  end
-=end
 end
